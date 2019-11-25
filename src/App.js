690a480e3c95b720cb72c56e092436c9dca37dfb
Alex Wensley <cwensley@gmail.com>
2019-11-25 13:07:20 -0700

    added hash routing package

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React,{useState} from "react";
 import "./App.css"
-import {Router,Link} from "@reach/router";
+import createHashSource from 'hash-source';
+import {Router,Link,createHistory,LocationProvider} from "@reach/router";
 import SearchParams from "./organisms/SearchParams";
 import Details from "./organisms/Details";
 import ThemeContext from "./atoms/ThemeContext";
 
+let source = createHashSource();
+let history = createHistory(source);
+
 const App = () => {
       const themeHook = useState('peru');
       return (
-          <ThemeContext.Provider value={themeHook}>
-            <div>
-                <header>
-                    <Link to="/">Adopt Me!</Link>
-                </header>
-                <Router>
-                    <SearchParams path="/" />
-                    <Details path="/details/:id" />
-                </Router>
-            </div>
-          </ThemeContext.Provider>
+          <LocationProvider history={history}>
+              <ThemeContext.Provider value={themeHook}>
+                <div>
+                    <header>
+                        <Link to="/">Adopt Me!</Link>
+                    </header>
+                    <Router>
+                        <SearchParams path="/" />
+                        <Details path="/details/:id" />
+                    </Router>
+                </div>
+              </ThemeContext.Provider>
+          </LocationProvider>
       );
 };
 
