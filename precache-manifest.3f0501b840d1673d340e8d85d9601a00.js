c4dd68d5144a0df55689e3265812174db5779d16
Alex Wensley <cwensley@gmail.com>
2019-11-25 12:13:52 -0700

    Updates

---
+++ precache-manifest.3f0501b840d1673d340e8d85d9601a00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c493457da5aa689ba449d5e9bfb60e7e",
+    "url": "/AdoptMe/index.html"
+  },
+  {
+    "revision": "ae47b19154d153a3b052",
+    "url": "/AdoptMe/static/css/main.4fae9d1f.chunk.css"
+  },
+  {
+    "revision": "6e9d2021d5c4471f8fa4",
+    "url": "/AdoptMe/static/js/2.50a05372.chunk.js"
+  },
+  {
+    "revision": "ae47b19154d153a3b052",
+    "url": "/AdoptMe/static/js/main.834ab660.chunk.js"
+  },
+  {
+    "revision": "415a3b29427271f091e4",
+    "url": "/AdoptMe/static/js/runtime-main.b10264ae.js"
+  }
+]);
