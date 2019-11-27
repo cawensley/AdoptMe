1ebb3b4ffd7ea08db0e811dab41ef91e2dc3fd89
Alex Wensley <cwensley@gmail.com>
2019-11-26 21:40:35 -0700

    removed extraneous css

--- src/molecules/Navbar.js
+++ src/molecules/Navbar.js
@@ -1,24 +1,17 @@
 import React from "react";
 import {Link} from '@reach/router';
 import {css,keyframes} from 'emotion';
-import colors from '../atoms/colors'
 
 const spin = keyframes`to {transform: rotate(360deg);}`;
 
 const NavBar = () => {
     return (
-    <header className={css`
-        background-color: ${colors.light};
-        padding: 15px;
-        `}>
+    <header>
         <Link to="/">Adopt Me!</Link>
         <span className={css`
             font-size: 60px;
-            display: inline-block;
-            &:hover {
-                text-decoration: underline;
-                animation: 1s ${spin} linear infinite;
-            `} role="img" aria-label="logo">🤪</span>
+            &:hover {animation: 1s ${spin} linear infinite;
+          `} role="img" aria-label="logo">🤪</span>
     </header>
     )
 };
